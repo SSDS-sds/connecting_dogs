41ab97cf430fb4ba734d3ac6b2adc25e6c5372cb
SSDS-sds <warwicklee1@gmail.com>
2025-03-15 18:07:11 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -26,12 +26,38 @@ def draw():
     screen.blit("bg",(0,0))
     num = 1
     for dog in dogs:
-        screen.draw.text(str(num),(dog.pos[0],dog.pos[1] + 40))
+        screen.draw.text(str(num),(dog.pos[0],dog.pos[1] + 35))
         dog.draw()
         num = num + 1
         
+    for line in lines:
+        screen.draw.line(line[0],line[1],(255,255,255))
+    
+    if current_dog < total_dog:
+        total_time = time() - start_time
+        screen.draw.text(str(round(total_time,1)), (10,10), fontsize = 30)
+    
+    else:
+        screen.draw.text(str(round(total_time,1)), (10,10), fontsize = 30)
+
 def update():
     pass
+
+def on_mouse_down(pos):
+    global current_dog,lines
+    if current_dog < total_dog:
+        if dogs[current_dog].collidepoint(pos):
+            if current_dog:
+                lines.append(
+                    (
+                        dogs[current_dog-1].pos,
+                        dogs[current_dog].pos
+                    )
+                )
+            current_dog = current_dog + 1
+        else:
+            lines = []
+            current_dog = 0
 
 
 
